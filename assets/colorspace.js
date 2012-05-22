ad85ada51bbcf91d84cee861a0a113c55a8d162c
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-05-22 06:38:15 +0100

    event triggered by form changes colour in grid

--- assets/colorspace.js
+++ assets/colorspace.js
@@ -1,7 +1,22 @@
-function ColorspaceCtrl($scope) {
+'use strict';
+
+function GridCtrl($scope, $rootScope) {
 
   $scope.baseColors = [
-    { color: '#ffaa66' }
+    { color: '#ffaa66' },
+    { color: '#f900f9' }
   ];
 
+  $rootScope.$on('base-color-change', function(event) {
+  	$scope.baseColors[0].color = event.targetScope.color;
+  });
+
+}
+
+function BaseColorFormCtrl($scope) {
+	$scope.color = '#ffaa66';
+	$scope.changeColor = function() {
+		console.log('new color', $scope.color, $scope);
+		$scope.$emit('base-color-change', $scope.color);
+	}
 }
